7119ae270fc67ea2e870077b7f327afd1f8a8cd4
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-28 05:51:18 +0300

    Refactoring

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -5,40 +5,68 @@ namespace App;
 class BowlingGame
 {
     const FRAMES_PER_GAME = 10;
+    const MAX_SCORE_PER_FRAME = 10;
 
-    protected array $rolls = [];
+    private array $rolls = [];
 
-    public function roll(int $pins)
+    public function roll(int $pins): void
     {
         $this->rolls[] = $pins;
     }
 
-    public function score()
+    public function score(): int
     {
         $score = 0;
         $roll = 0;
 
         foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
-            if ($this->rolls[$roll] === 10) {
-                // we got a strike
-                $score += $this->rolls[$roll];
-                $score += $this->rolls[$roll + 1];
-                $score += $this->rolls[$roll + 2];
+            if ($this->isStrike($roll)) {
+                $score += $this->pinCount($roll) + $this->strikeBonus($roll);
 
                 $roll += 1;
-            } else if ($this->rolls[$roll] + $this->rolls[$roll + 1] === 10) {
-                // we got a spare
-                $score += $this->rolls[$roll] + $this->rolls[$roll + 1];
-                $score += $this->rolls[$roll + 2];
 
-                $roll += 2;
-            } else {
-                $score += $this->rolls[$roll] + $this->rolls[$roll + 1];
-                
-                $roll += 2;
+                continue;
             }
+
+            $score += $this->defaultFrameScore($roll);
+            
+            if ($this->isSpare($roll)) {
+                $score += $this->spareBonus($roll);
+            }
+                
+            $roll += 2;
         }
 
         return $score;
+    }
+
+    private function isStrike(int $roll): bool
+    {
+        return $this->pinCount($roll) === self::MAX_SCORE_PER_FRAME;
+    }
+
+    private function isSpare(int $roll): bool
+    {
+        return $this->defaultFrameScore($roll) === self::MAX_SCORE_PER_FRAME;
+    }
+
+    private function defaultFrameScore(int $roll): int
+    {
+        return $this->pinCount($roll) + $this->pinCount($roll + 1);
+    }
+
+    private function strikeBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 1) + $this->pinCount($roll + 2);
+    }
+
+    private function spareBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 2);
+    }
+
+    private function pinCount(int $roll): int
+    {
+        return $this->rolls[$roll];
     }
 }
